fe770b4b90ffadfde350f1dbab449b0febd7397f
SeungYongShim <syshim@me.com>
2023-03-12 23:13:47 +0900

    .

--- src/ConsoleApp1/ConsoleActor.cs
+++ src/ConsoleApp1/ConsoleActor.cs
@@ -1,11 +1,39 @@
-﻿using Proto;
+using Medallion.Threading.Redis;
+using Proto;
+using StackExchange.Redis;
 
-public class ConsoleActor : IActor
+public class ConsoleActor : IActor, IAsyncDisposable
 {
-    public Task ReceiveAsync(IContext context) => context.Message switch
+    public ConsoleActor(IDatabase connection)
     {
-        Started => Task.Factory.StartNew(() => context.SetReceiveTimeout(TimeSpan.FromSeconds(3))),
-        ReceiveTimeout => Task.Factory.StartNew(() => context.Send(context.Parent, new ProxyPoisonTarget(context.Self))),
-        { } msg => Task.Factory.StartNew(() => Console.WriteLine(msg))
-    };
+        Database = connection;
+    }
+
+    public IDatabase Database { get; }
+    public RedisDistributedLockHandle? Handle { get; private set; }
+
+    public async ValueTask DisposeAsync()
+    {
+        var t = Handle?.DisposeAsync() ?? ValueTask.CompletedTask;
+        await t;
+    }
+
+    public async Task ReceiveAsync(IContext context) 
+    {
+        var task = context.Message switch
+        {
+            Started => Task.Factory.StartNew(async () =>
+            {
+                var @lock = new RedisDistributedLock(context.Self.ToString(), Database);
+                Handle = await @lock.AcquireAsync(TimeSpan.FromSeconds(3));
+
+                context.SetReceiveTimeout(TimeSpan.FromSeconds(3));
+            }),
+            ReceiveTimeout => Task.Factory.StartNew(() => context.Send(context.Parent!, new ProxyPoisonTarget(context.Self))),
+            { } v => Task.Factory.StartNew(() => Console.WriteLine(v)),
+            _ => Task.CompletedTask
+        };
+
+        await task;
+    }
 }
--- src/ConsoleApp1/Program.cs
+++ src/ConsoleApp1/Program.cs
@@ -1,13 +1,27 @@
+using Medallion.Threading.Redis;
+using Microsoft.Extensions.Logging;
 using Proto;
+using Proto.Logging;
+using StackExchange.Redis;
 
-var system = new ActorSystem();
+
+
+
+
+var connection = await ConnectionMultiplexer.ConnectAsync("localhost"); // uses StackExchange.Redis
+var dbRedis = connection.GetDatabase();
+
+var system = new ActorSystem(new ActorSystemConfig
+{
+    DiagnosticsLogLevel = Microsoft.Extensions.Logging.LogLevel.Trace
+});
 
 var root = system.Root;
 
 var props = Props.FromProducer(() =>
                 new ProxyActor(Props.FromProducer(() =>
                     new ProxyActor(Props.FromProducer(() =>
-                        new ConsoleActor())))));
+                        new ConsoleActor(dbRedis))))));
 
 var pid = root.SpawnNamed(props, nameof(ProxyActor));
 
@@ -19,7 +33,7 @@ await Task.Delay(3100);
 root.Send(pid, new MessageId("1"));
 root.Send(pid, new MessageId("1"));
 
-await Task.Delay(5000);
+await Task.Delay(6000);
 
 await system.ShutdownAsync();
 
